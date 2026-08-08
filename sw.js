bbb2e23a4e9f029e3d9a38e097b7f1fe19c5fdcb
Jumpy2324 <gianmarco.candeletti@hotmail.it>
2026-08-08 02:59:27 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const CACHE="space16154-v6";
+self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./app.js?v=6.0.0","./styles.css?v=6.0.0"]))));
+self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
+self.addEventListener("fetch",e=>{
+  if(e.request.method!=="GET") return;
+  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
+});
