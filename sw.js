80abd03d5dd0300a155ea237a74f04fbb971b7fa
Jumpy2324 <gianmarco.candeletti@hotmail.it>
2026-08-08 02:40:56 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,7 +0,0 @@
-const CACHE="space16154-v5";
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(["./","./index.html","./app.js?v=5.0.0","./styles.css?v=5.0.0"]))));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET") return;
-  e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request)));
-});
